6c6fded55e091dea9c6a77f777fb969a6f0794d7
Anirban Roy <aroyiem@gmail.com>
2021-02-26 11:38:32 +0000

    added rest api for fetching customers

--- src/main/java/com/roy/gateway/config/GoogleConfig.java
+++ src/main/java/com/roy/gateway/config/GoogleConfig.java
@@ -3,18 +3,17 @@ package com.roy.gateway.config;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
-@Configuration
-@Profile("google")
+//@Configuration
+//@Profile("google")
 public class GoogleConfig {
 
     @Bean
-    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder){
+    public RouteLocator googleRouteConfig(RouteLocatorBuilder builder) {
         return builder.routes()
                 .route(r -> r.path("/googlesearch")
                         .filters(f -> f.rewritePath("/googlesearch(?<segment>/?.*)", "/${segment}"))
-                        .uri("https://google.com")).build();
+                        .uri("https://google.com"))
+                .build();
     }
 }
